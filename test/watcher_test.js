a6e69781551658264e5c34157e568a99a1d509c9
Santiago Lezica <slezica89@gmail.com>
2016-07-24 00:59:36 -0300

    Fix temp file management in watcher test

--- test/watcher_test.js
+++ test/watcher_test.js
@@ -14,67 +14,71 @@ import watcher from '../src2/watcher'
 describe("The file watcher", function() {
   this.timeout(5000)
 
-  let temp
+  let tempRoot = mkdtempSync('./')
 
   beforeEach(function() {
-    temp = mkdtempSync('./')
+    this.temp = mkdtempSync(`${tempRoot}/`)
 
-    mkdirSync(`${temp}/sub`)
-    touch.sync(`${temp}/foo`)
+    mkdirSync(`${this.temp}/sub`)
+    touch.sync(`${this.temp}/foo`)
 
     this.callback = sinon.spy()
-    watcher.start(temp, this.callback)
+    watcher.start(this.temp, this.callback)
 
     return sleep(100) // watcher has no 'ready' event
   })
 
   afterEach(function() {
     watcher.stop()
-    rmRf.sync(temp)
+    rmRf.sync(this.temp)
+  })
+
+  after(function() {
+    rmRf.sync(tempRoot)
   })
 
   it("should detect a changed file", function() {
-    touch.sync(`${temp}/foo`)
+    touch.sync(`${this.temp}/foo`)
     return wait().then(() => expect(this.callback).called)
   })
 
   it("should detect a new file", function() {
-    touch.sync(`${temp}/newfoo`)
+    touch.sync(`${this.temp}/newfoo`)
     return wait().then(() => expect(this.callback).called)
   })
 
   it("should detect a new directory", function() {
-    mkdirSync(`${temp}/newsub`)
+    mkdirSync(`${this.temp}/newsub`)
     return wait().then(() => expect(this.callback).called)
   })
 
   it("should detect changes inside new directories", function() {
     return this.skip() // NOT IMPLEMENTED YET
 
-    mkdirSync(`${temp}/newsub`)
-    touch.sync(`${temp}/newsub/newfoo`)
+    mkdirSync(`${this.temp}/newsub`)
+    touch.sync(`${this.temp}/newsub/newfoo`)
     return wait().then(() => expect(this.callback).calledTwice)
   })
 
   it("should detect events in subpaths", function() {
-    touch.sync(`${temp}/sub/bar`)
+    touch.sync(`${this.temp}/sub/bar`)
     return wait().then(() => expect(this.callback).called)
   })
 
   it("should group near-simultaneous events", function() {
-    touch.sync(`${temp}/foo`)
-    touch.sync(`${temp}/sub/foo`)
-    touch.sync(`${temp}/sub/bar`)
+    touch.sync(`${this.temp}/foo`)
+    touch.sync(`${this.temp}/sub/foo`)
+    touch.sync(`${this.temp}/sub/bar`)
     return wait().then(() => expect(this.callback).calledOnce)
   })
 
   it("should not wait too much for events to settle", function() {
     this.timeout(2000)
-    touch.sync(`${temp}/foo`)
-    touch.sync(`${temp}/sub/bar`)
+    touch.sync(`${this.temp}/foo`)
+    touch.sync(`${this.temp}/sub/bar`)
     
     return waitTooMuch()
-      .then(() => touch.sync(`${temp}/sub/foo`))
+      .then(() => touch.sync(`${this.temp}/sub/foo`))
       .then(wait)
       .then(() => expect(this.callback).calledTwice)
   })
