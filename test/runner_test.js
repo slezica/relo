ec406e15d190e7918298756ee455010b65224654
Nicolas Santangelo <santangelonicolas@gmail.com>
2024-06-23 13:32:16 -0300

    Replace notCalled with .not.called

--- test/runner_test.js
+++ test/runner_test.js
@@ -45,7 +45,7 @@ describe("The process Runner", function() {
       runner = new MockRunner({ wait: true })
       runner.spawn()
       runner.respawn()
-      expect(runner.kill).notCalled
+      expect(runner.kill).not.called
       
       runner = new MockRunner({ wait: false })
       runner.spawn()
@@ -85,7 +85,7 @@ describe("The process Runner", function() {
       runner.spawn()
       runner.kill()
       expect(runner.killGroup).called
-      expect(runner.killSingle).notCalled
+      expect(runner.killSingle).not.called
 
       runner.killGroup.resetHistory()
       runner.killSingle.resetHistory()
@@ -93,7 +93,7 @@ describe("The process Runner", function() {
       runner = new MockRunner({ group: false })
       runner.spawn()
       runner.kill()
-      expect(runner.killGroup).notCalled
+      expect(runner.killGroup).not.called
       expect(runner.killSingle).called
     })
   })
