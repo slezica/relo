845dc6b78a43c88c3ebdec6a4afb8e134468e3fb
Nicolas Santangelo <santangelonicolas@gmail.com>
2024-06-23 12:29:42 -0300

    Fix respawn test

--- test/runner_test.js
+++ test/runner_test.js
@@ -32,7 +32,7 @@ describe("The process Runner", function() {
 
   describe("#respawn", function() {
     const MockRunner = mockRunnerClass('spawn', 'kill')
-
+    
     it("should call spawn() if no process is running", function() {
       const runner = new MockRunner()
       runner.respawn()
@@ -57,14 +57,18 @@ describe("The process Runner", function() {
       let runner
 
       runner = new MockRunner({ parallel: true })
+      runner.spawn.resetHistory()
       runner.spawn()
       runner.respawn()
-      expect(runner.spawn).called
+      expect(runner.spawn).calledTwice
 
+      runner.spawn.resetHistory()
+      
       runner = new MockRunner({ parallel: false })
       runner.spawn()
       runner.respawn()
-      expect(runner.spawn).notCalled
+
+      expect(runner.spawn).calledOnce
       expect(runner.process.on).calledTwice
       expect(runner.process.on.alwaysCalledWith('exit')).to.be.true
     })
@@ -82,6 +86,9 @@ describe("The process Runner", function() {
       runner.kill()
       expect(runner.killGroup).called
       expect(runner.killSingle).notCalled
+
+      runner.killGroup.resetHistory()
+      runner.killSingle.resetHistory()
 
       runner = new MockRunner({ group: false })
       runner.spawn()
